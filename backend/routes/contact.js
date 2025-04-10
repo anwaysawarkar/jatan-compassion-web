ae09ec9e5d676b31486027859247e4a92a90f8a0
APURV ANAND <77606488+Ichinose11@users.noreply.github.com>
2025-04-10 18:18:23 +0530

    Update contact.js

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -21,6 +21,11 @@ router.post('/send-message', async (req, res) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+
+    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+  console.error('Email user or password is not defined.');
+  // Handle the error appropriately
+}
   });
 
   // Determine subject based on form type
