42fc465c89daee5c9b333db192ea04e99ff7f944
APURV ANAND <77606488+Ichinose11@users.noreply.github.com>
2025-04-10 19:26:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ import contactRoutes from './routes/contact.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -15,7 +14,3 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
