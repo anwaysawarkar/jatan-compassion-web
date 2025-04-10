a0f0fd1fa007858904dc79162e72483ce7391ccf
APURV ANAND <77606488+Ichinose11@users.noreply.github.com>
2025-04-10 18:33:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import contactRoutes from './routes/contact.js';
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
